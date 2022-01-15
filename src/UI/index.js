05b9745e4ef134caead17f4683e39494aec6dcbb
MrPluto0 <710852553@qq.com>
2022-01-15 13:43:27 +0800

    colors

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,23 +1,17 @@
 //引入组件
 
-
 // 组件列表
-const allcomponents=[
-  
-]
-
+const allcomponents = [];
 
 //组件注册
-const install=function(Vue){
-    // 遍历并注册所有组件
-    allcomponents.map(component =>{
-       Vue.component(component.name, component);
-    })
-
-}
-
+const install = function (Vue) {
+  // 遍历并注册所有组件
+  allcomponents.map((component) => {
+    Vue.component(component.name, component);
+  });
+};
 
 //导出全局install
 export default {
-    install
-}
+  install,
+};
