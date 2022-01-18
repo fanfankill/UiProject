2b567c20f65b445bac6b175293791da4864f4c7d
coder-lbw <3113219455@qq.com>
2022-01-18 09:54:42 +0800

    lbw

--- src/components/asider/index.vue
+++ src/components/asider/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(item, index) in Uiname" :key="index">
-        {{ item.name }}
-      </li>
-    </ul>
+    <vue-scroll>
+      <ul>
+        <li v-for="(item, index) in sideNav" :key="index">
+          <router-link :to="item.eName" tag="li" active-class="ct-aside-active">{{`${item.name} ${item.eName}`}}</router-link>
+        </li>
+      </ul>
+    </vue-scroll>
   </div>
 </template>
 
@@ -12,13 +14,145 @@
 export default {
   data() {
     return {
-      Uiname: [
+      sideNav: [
         {
           name: "按钮",
+          eName: 'button',
           href: "#",
         },
         {
           name: "链接",
+          eName: 'link',
+          href: "#",
+        },
+        {
+          name: "卡片",
+          eName: 'card',
+          href: "#",
+        },
+        {
+          name: "滚动",
+          eName: 'roll',
+          href: "#",
+        },
+        {
+          name: "面包屑",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "滑块",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "气泡",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "模态框",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "输入框",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "评分",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "表格",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "标签",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "提示",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "弹出框",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "按钮",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "链接",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "卡片",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "滚动",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "面包屑",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "滑块",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "气泡",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "模态框",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "输入框",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "评分",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "表格",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "标签",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "提示",
+          eName: 'button',
+          href: "#",
+        },
+        {
+          name: "弹出框",
+          eName: 'button',
           href: "#",
         },
       ],
@@ -27,13 +161,25 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.ct-aside-active {
+  color: #62bae2;
+  background-color: #f4f8fa;
+}
 ul {
-  width: 100%;
+  width: 88%;
+  margin: 10px auto;
 }
 ul li {
   height: 40px;
   line-height: 40px;
   text-align: center;
 }
+ul li+li {
+  margin-top: 4px;
+}
+ul li:hover {
+  background-color: #f4f7f8;
+}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ Vue.use(VueRouter);
 //路由引入
 const mainView = () => import("../views/main.vue");
 const styleView = () => import("../views/style.vue");
+const Button = () => import("../components/button/index")
+const Link = () => import("../components/link/index")
 
 const routes = [
   {
@@ -15,6 +17,14 @@ const routes = [
     path: "/style",
     component: styleView,
   },
+  {
+    path: "/button",
+    component: Button
+  },
+  {
+    path: "/link",
+    component: Link
+  }
 ];
 
 //默认hash
