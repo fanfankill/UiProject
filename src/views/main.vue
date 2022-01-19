36bdc0c9e69b07c880ec5180256cf70f4a624021
MrPluto0 <710852553@qq.com>
2022-01-19 18:47:05 +0800

    update:color

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="container">
     <ct-button></ct-button>
-    <form action="" class="form">
-      <button class="form__btn--main"></button>
-    </form>
   </div>
 </template>
 
