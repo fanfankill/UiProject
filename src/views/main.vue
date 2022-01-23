b2c2a2f068461a4ee72f4ddf739e37bd68a329c5
‘haoyue’ <1762351793@qq.com>
2022-01-23 13:47:07 +0800

    update:view/main

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,28 +1,6 @@
 <template>
   <div class="mycontainer">
-    <!-- <show-component 
-    title="基础用法" 
-    subtitle="按钮的基础用法" 
-    :attributes="['type, size']" 
-    ctname="Button"
-    codes="
-<h>12</h>s
-   <span>12</span>
-   <h>12</h>s
-   <span>12</span><h>12</h>s
-   <span>12</span>
-   <h>12</h>s
-   <span>12</span><h>12</h>s
-   <span>12</span>
-   <h>12</h>s
-   <span>12</span><h>12</h>s
-   <span>12</span>
-    <h>12</h>s
-   <span>12</span>">
-       <template v-slot:showarea>
-       <ct-container>
-       </template>
-    </show-component> -->
+
     <router-view></router-view>
   </div>
 </template>
@@ -37,15 +15,12 @@ export default {
 }
 </script>
 <style scoped lang="less">
-.container {
+.mycontainer {
   border: 2px solid @vice-color1;
   background-color: @blue-bg-color1;
-  }
-
-.mycontainer {
-  // border: 2px solid @vice-color1;
-  // background-color: @blue-bg-color1;
   padding-bottom: 80px;
 }
+
+
 </style>
 
