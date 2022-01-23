551009d698ea4243051239ba8aa6a7673787a860
‘haoyue’ <1762351793@qq.com>
2022-01-23 13:59:39 +0800

    updata

--- src/views/main.vue
+++ src/views/main.vue
@@ -13,24 +13,6 @@
       </ct-container>
       </ct-container>
     </showdiv> -->
-    <div class="box">
-      <div class="row">
-        <ct-skeleton  type="circle" color="green" size="100px" animated> 圆</ct-skeleton>
-        <div style="width:200px">
-          <ct-skeleton>默认</ct-skeleton>
-          <ct-skeleton  type="text" color="pink">文本 </ct-skeleton>
-        </div>
-      </div>
-       <ct-skeleton  type="rect" color="skyblue" animated>矩形 </ct-skeleton>
-      <div class="column">
-        <ct-skeleton  type="badge" color="red"> 标记</ct-skeleton>
-        <ct-skeleton  type="btn" color="skyblue"> 按钮</ct-skeleton>
-        <ct-skeleton  type="chip" color="yellow">Chip</ct-skeleton>
-      </div>
-     
-    </div>
-     
-     <!-- <ct-badge  :value="12" type="success"></ct-badge> -->
   </div>
 </template>
 
@@ -51,25 +33,5 @@ export default {
 //   // border: 2px solid @vice-color1;
 //   // background-color: @blue-bg-color1;
 // }
-.box{
-  width: 300px;
-  height: 400px;
-  padding: 20px;
-  border: 1px solid #ccc;
-}
-.showdiv{
-  height: 400px;
-  width: 80%;
-}
-.row{
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 20px;
-}
-.column{
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
 
-}
 </style>
