848792955a26b63ed552f9c6a13129ddd4f51006
MrPluto0 <710852553@qq.com>
2022-01-31 00:02:11 +0800

    update:main.vue

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,67 +1,13 @@
 <template>
-  <div class="mycontainer">
-    <!-- <show-component 
-    title="基础用法" 
-    subtitle="按钮的基础用法" 
-    :attributes="['type, size']" 
-    ctname="Button"
-    codes="
-<h>12</h>s
-   <span>12</span>
-   <h>12</h>s
-   <span>12</span><h>12</h>s
-   <span>12</span>
-   <h>12</h>s
-   <span>12</span><h>12</h>s
-   <span>12</span>
-   <h>12</h>s
-   <span>12</span><h>12</h>s
-   <span>12</span>
-    <h>12</h>s
-   <span>12</span>">
-       <template v-slot:showarea>
-       <ct-container>
-       </template>
-    </show-component> -->
-<<<<<<< HEAD
-
-    <router-view> </router-view>
-=======
+  <div class="ontainer">
     <router-view></router-view>
->>>>>>> ffc908d72a99b0c9140b88181a0b317809f2cdc2
   </div>
 </template>
 
-<script>
-<<<<<<< HEAD
-export default {};
-=======
-// import showdiv from '../components/showcompont'
-export default {
-    name:'showcompont',
-    // components:{
-    //   showdiv
-    // }
-}
->>>>>>> ffc908d72a99b0c9140b88181a0b317809f2cdc2
-</script>
+<script></script>
+
 <style scoped lang="less">
 .container {
   border: 2px solid @vice-color1;
-  background-color: @blue-bg-color1;
 }
-
-.mycontainer {
-  // border: 2px solid @vice-color1;
-  // background-color: @blue-bg-color1;
-  padding-bottom: 80px;
-}
-<<<<<<< HEAD
-
-.showdiv {
-  height: 400px;
-  width: 80%;
-}
-=======
->>>>>>> ffc908d72a99b0c9140b88181a0b317809f2cdc2
 </style>
