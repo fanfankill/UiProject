7b58e9afe3ace12e63c5d90b57d9a634b4d39b90
fanfankill <1623920276@qq.com>
2022-02-09 13:18:54 +0800

    2.9

--- src/router/index.js
+++ src/router/index.js
@@ -14,13 +14,17 @@ const styleView = () => import("../views/style.vue");
 
 //组件展示路由引入
 const avatar = () => import("../examples/avatar");
+const button = () => import("../examples/button");
 const chatbox = () => import("../examples/chatbox");
 const container = () => import("../examples/container");
+const descriptions = () => import("../examples/descriptions");
 const dialog = () => import("../examples/dialog");
 const dropdown = () => import("../examples/dropdown");
+const radio = () => import("../examples/radio");
 const loading = () => import("../examples/loading");
 const scrollbar = () => import("../examples/scrollbar");
 const steps = () => import("../examples/steps");
+
 
 
 const routes = [
@@ -40,11 +44,17 @@ const routes = [
         path: "avatar",
         component: avatar,
       },{
+        path: "button",
+        component: button,
+      },{
         path: "chatbox",
         component: chatbox,
       },{
         path: "container",
         component: container,
+      },{
+        path: "descriptions",
+        component: descriptions,
       },{
         path: "dialog",
         component: dialog,
@@ -54,6 +64,9 @@ const routes = [
       },{
         path: "dropdown",
         component: dropdown,
+      },{
+        path: "radio",
+        component: radio,
       },{
         path: "scrollbar",
         component: scrollbar,
